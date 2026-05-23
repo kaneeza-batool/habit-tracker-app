784036754a02c9dedc36adb6e3fcc489db5b41d0
Kaneeza Batool <kaneezamem847@gmail.com>
2026-05-23 23:15:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -437,5 +437,35 @@ document
 
 document.getElementById("saveNote").addEventListener("click", saveLog);
 
+const menuToggle = document.getElementById("menuToggle");
+const sidebar = document.getElementById("sidebar");
+
+menuToggle.addEventListener("click", (e) => {
+  e.stopPropagation();
+  sidebar.classList.toggle("open");
+});
+
+sidebar.addEventListener("click", (e) => {
+  e.stopPropagation();
+
+  if (
+    e.target.closest(".snav-btn") ||
+    e.target.closest("button") ||
+    e.target.closest("a")
+  ) {
+    sidebar.classList.remove("open");
+  }
+});
+
+document.addEventListener("click", (e) => {
+    
+  if (
+    sidebar.classList.contains("open") &&
+    !sidebar.contains(e.target) &&
+    !menuToggle.contains(e.target)
+  ) {
+    sidebar.classList.remove("open");
+  }
+});
 loadData();
 render();
